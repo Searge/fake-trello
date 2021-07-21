6858d125408f9cbdb435d46b88a834bd3858f3e2
Searge <ashikcygan@gmail.com>
2021-07-21 08:30:07 +0300

    :tada:

---
+++ database/factories/TeskFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tesk;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TeskFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Tesk::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
