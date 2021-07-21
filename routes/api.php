b8e88d36d07fc6b730052446ac1029c00a824053
Searge <ashikcygan@gmail.com>
2021-07-21 13:42:45 +0300

    Return JSON from Desks:DB --api

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/desks', [DeskController::class, 'index']);
+Route::apiResources([
+    'desks' => DeskController::class,
+]);
