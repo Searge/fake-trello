194cc4419751ad4f661ff9edbde66dce604d988a
Searge <ashikcygan@gmail.com>
2021-07-23 06:29:36 +0300

    pages: Home | About

---
+++ resources/js/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <p>
+            <router-link :to="{ name: 'home' }">Home</router-link>
+            <router-link :to="{ name: 'about' }">About</router-link>
+        </p>
+        <div>
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        name: 'App';
+    },
+};
+</script>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,10 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fake Trello</title>
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 
 <body>
-    <div id="app"></div>
+    <div id="app">
+        <app></app>
+    </div>
+    <script src="{{mix('js/app.js')}}"></script>
 </body>
 
 </html>
